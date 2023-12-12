49269f4aa468ee9efc29e8acb641495ed9050988
SokolovVasily <ber-sokolov-vasily@outlook.de>
2023-12-12 17:53:25 +0100

    Initial commit

---
+++ src/main/java/com/vasily_sokolov/nucacola/NucaColaApplication.java
@@ -0,0 +1,13 @@
+package com.vasily_sokolov.nucacola;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NucaColaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NucaColaApplication.class, args);
+    }
+
+}
